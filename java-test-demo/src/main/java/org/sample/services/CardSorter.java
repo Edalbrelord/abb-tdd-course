a306b968f47b4901d292660ee4be16485071cb18
Edalbrelord <edalbrelord@gmail.com>
2016-06-22 23:51:50 +0200

    Gitignore

--- java-test-demo/src/main/java/org/sample/services/CardSorter.java
+++ java-test-demo/src/main/java/org/sample/services/CardSorter.java
@@ -19,8 +19,79 @@ public class CardSorter {
      */
     public String[] sort(String[] shuffledCards) {
 
-//        TODO: Objective1 - complete this method
+        String[] sortedCards = new String[52];
 
-        return shuffledCards;
+        for (String card : shuffledCards) {
+            char suit = card.charAt(0);
+            String value = card.substring(1);
+
+            int position = getPosition(suit, value);
+
+            if (position > 52) {
+                System.out.println(position + ": " + card);
+            } else {
+                sortedCards[position - 1] = card;
+            }
+        }
+
+        return sortedCards;
+    }
+
+    private int getPosition(char suit, String value) {
+        int position;
+
+//        Determine position of suit
+        switch (suit) {
+            case '♠':
+                position = 0;
+                break;
+
+            case '♥':
+                position = 13;
+                break;
+
+            case '♦':
+                position = 26;
+                break;
+
+            case '♣':
+                position = 39;
+                break;
+
+            default:
+                throw new IllegalArgumentException("Suit should be ♠, ♥, ♦ or ♣");
+        }
+
+//        Determine position of value
+        switch (value) {
+            case "A":
+                position += 1;
+                break;
+            case "K":
+                position += 2;
+                break;
+            case "Q":
+                position += 3;
+                break;
+            case "J":
+                position += 4;
+                break;
+            default:
+                try {
+
+//                position for 10 = 5, 9 = 6, etc.. Therefore, start at 15
+                    int number = Integer.parseInt(value);
+                    if(number > 10 || number < 2){
+                        throw new IllegalArgumentException("Value should be between 2 and 10");
+                    }
+                    position += (15 - number);
+                    break;
+                }
+                catch(NumberFormatException nfe){
+                    throw new IllegalArgumentException("Value should be an integer");
+                }
+        }
+
+        return position;
     }
 }
